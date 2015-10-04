bc030e8d291980cb73713b128d49990809669a7b
ongzlalan <ongzl.alan@gmail.com>
2015-10-04 20:40:28 +0800

    Revert "Added specific commands for each function"
    This reverts commit e7bad4d6b05d0ee7e44e0b477c6c5aa51d25a4ad.

--- Command.java
+++ Command.java
@@ -2,30 +2,10 @@ public class Command {
   
   private Tasks task;
   private String command;
-  private String searchKeyword;
-  private int indexToBeDeleted;
   
-  //for adding and updating
   public Command(String command, Tasks task) {
     this.command = command;
     this.task = task;
-  }
-  
-  //for searching
-  public Command(String command, String searchKeyword){
-	  this.command = command;
-	  this.searchKeyword = searchKeyword;
-  }
-  
-  //for deleting
-  public Command(String command, int indexToBeDeleted){
-	  this.command = command;
-	  this.indexToBeDeleted = indexToBeDeleted;
-  }
-  
-  //for exit and invalid commands
-  public Command(String command){
-	  this.command = command;
   }
   
   public Tasks getTask(){
@@ -35,13 +15,4 @@ public class Command {
   public String getCommand(){
 	  return command;
   }
-  
-  public String getSearchKeyword() {
-	  return searchKeyword;
-  }
-
-  public int getIndexToBeDeleted() {
-	  return indexToBeDeleted;
-  }
-  
 }
